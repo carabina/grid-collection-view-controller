62997336e6034c90371bfb4b28d5e5889894917f
Julien Sarazin <julien.sarazin@me.com>
2016-12-26 12:05:35 +0100

    updated samples

---
+++ Samples/DGGridCollectionViewControllerSample-tvOS/DGGridCollectionViewControllerSample-tvOS/UserItemCell.swift
@@ -0,0 +1,27 @@
+//
+//  UserItemCell.swift
+//  PaginableTableView
+//
+//  Created by Julien Sarazin on 19/12/2016.
+//  Copyright © 2016 Julien Sarazin. All rights reserved.
+//
+
+import UIKit
+
+class UserItemCell: UICollectionViewCell {
+	@IBOutlet weak var nameLabel: UILabel!
+	@IBOutlet weak var companyLabel: UILabel!
+
+	var indexPath: IndexPath?
+	static let Identifier = "UserItemCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+	func set(user: User) {
+		self.nameLabel.text = "\(indexPath!.row)"
+		self.companyLabel.text = user.company
+	}
+}
